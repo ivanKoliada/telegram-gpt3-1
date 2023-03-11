2b5bd216089224fe1b5bbb821bf16856ceeab5db
Ivan Koliada <ivanKoliada69@gmail.com>
2023-03-11 21:28:30 +0300

    feat: delete whatsapp

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { whatsapp } from "./lib/whatsapp.js";
 import { Messages, generate } from "./lib/openai.js";
 import { redisMethods } from "./lib/redis.js";
 import { telegram } from "./lib/telegram.js";
-import qrcode from "qrcode-terminal";
 
 telegram.on("message", async (msg) => {
   if (!msg.text) return;
@@ -41,52 +39,3 @@ telegram.on("message", async (msg) => {
 
 telegram.on("error", (err) => console.log(err.message));
 
-// _________________________________________________________________whatsapp under this lines
-
-whatsapp.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
-});
-
-whatsapp.on("ready", () => {
-  console.log("Whatsapp client is ready!");
-});
-
-whatsapp.initialize();
-
-// messages only trigger ai generation by the prefix of "bot:"
-whatsapp.on("message", async (msg) => {
-  if (!msg.body.toLowerCase().startsWith("bot:")) return;
-
-  const { id } = msg.id;
-
-  const { get, set } = await redisMethods();
-
-  const messages = await get(id);
-
-  if (!messages) {
-    const initialGeneration = [
-      { role: "system", content: "You are a helpful assistant" },
-      { role: "user", content: msg.body.replace("bot:", "")  },
-    ] satisfies Messages;
-    const generation = await generate(initialGeneration);
-    msg.reply(generation.message);
-    set(id, [
-      ...initialGeneration,
-      { role: "assistant", content: generation.message },
-    ]);
-    return;
-  }
-
-  const nextMessages = [
-    ...messages,
-    { role: "user", content: msg.body.replace("bot:", "") },
-  ] satisfies Messages;
-  const generation = await generate(nextMessages);
-
-  msg.reply(generation.message);
-
-  set(id, [
-    ...nextMessages,
-    { role: "assistant", content: generation.message },
-  ]);
-});
--- src/lib/whatsapp.ts
+++
@@ -1,5 +0,0 @@
-import { Client } from "whatsapp-web.js";
-
-export const whatsapp = new Client({});
-
-
