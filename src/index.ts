150f63716620453d00aa9f2fd713086f02e25a32
Ivan Koliada <ivanKoliada69@gmail.com>
2023-03-12 11:41:23 +0300

    feat: bot typing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { Messages, generate } from "./lib/openai.js";
-import { redisMethods } from "./lib/redis.js";
-import { telegram } from "./lib/telegram.js";
+import { Messages, generate } from './lib/openai.js';
+import { redisMethods } from './lib/redis.js';
+import { telegram } from './lib/telegram.js';
 
-telegram.on("message", async (msg) => {
+telegram.on('message', async (msg) => {
+  console.log(msg);
   if (!msg.text) return;
   const { id } = msg.chat;
 
@@ -12,30 +13,28 @@ telegram.on("message", async (msg) => {
 
   if (!messages) {
     const initialGeneration = [
-      { role: "system", content: "You are a helpful assistant" },
-      { role: "user", content: msg.text },
+      { role: 'system', content: 'You are a helpful assistant' },
+      { role: 'user', content: msg.text },
     ] satisfies Messages;
     const generation = await generate(initialGeneration);
     telegram.sendMessage(id, generation.message);
-    set(id, [
-      ...initialGeneration,
-      { role: "assistant", content: generation.message },
-    ]);
+    set(id, [...initialGeneration, { role: 'assistant', content: generation.message }]);
     return;
   }
 
-  const nextMessages = [
-    ...messages,
-    { role: "user", content: msg.text },
-  ] satisfies Messages;
+  const nextMessages = [...messages, { role: 'user', content: msg.text }] satisfies Messages;
   const generation = await generate(nextMessages);
 
   telegram.sendMessage(id, generation.message);
-  set(id, [
-    ...nextMessages,
-    { role: "assistant", content: generation.message },
-  ]);
+  set(id, [...nextMessages, { role: 'assistant', content: generation.message }]);
 });
 
-telegram.on("error", (err) => console.log(err.message));
+telegram.on('text', (msg) => {
+  telegram.sendChatAction(msg.chat.id, 'typing');
+});
 
+telegram.on('error', (err) => console.log(err.message));
+
+process.on('SIGINT', () => {
+  process.exit(0);
+});
